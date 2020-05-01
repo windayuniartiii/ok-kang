68ba2e62b4d19481596d35b3035cef0a1d7dbfb3
Miftahul1112 <62463737+Miftahul1112@users.noreply.github.com>
2020-05-01 15:03:38 +0700

    Menambahkan inputdata1.php
    Berfungsi untuk menghubungkan input data pada form sedang dengan database paket

---
+++ Web/inputdata1.php
@@ -0,0 +1,25 @@
+<?php
+ require_once('koneksii.php');
+ 
+ $permintaan = $_REQUEST['permintaan'];
+ $jumlah_tukang = $_REQUEST['jumlah_tukang'];
+ $waktu_pengerjaan = $_REQUEST['waktu_pengerjaan'];
+ $harga = $_REQUEST['harga'];
+ $alamat = $_REQUEST['alamat'];
+ $pemilik_properti = $_REQUEST['pemilik_properti'];
+ $jenis_paket = $_REQUEST['jenis_paket'];
+ 
+ // insert comment
+ $query = mysqli_query($db, "INSERT INTO paket ( permintaan, jumlah_tukang, waktu_pengerjaan, harga, alamat, pemilik_properti,jenis_paket) VALUES ('$permintaan','$jumlah_tukang','$waktu_pengerjaan','$harga','$alamat','$pemilik_properti','Sedang')");
+ 
+ if($query) {
+     echo json_encode(array( 'response'=>'success' ));
+ } else {
+     echo json_encode(array( 'response'=>'failed' ));
+ }
+?>
+
+
+
+
+
